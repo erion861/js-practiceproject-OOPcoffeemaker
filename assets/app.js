9b9ecbdefbfdcfe6c174e8a74308270ceeaf2080
Moe <67797901+erion861@users.noreply.github.com>
2021-03-08 19:23:14 +0000

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -5,18 +5,6 @@ const startBtn = document.getElementById('make');
 const clear = document.getElementById('clear');
 const input = document.getElementById('coffee-type');
 
-// SWITCH
-
-function toggle() {
-  if (toggleBtn.innerHTML === 'Off'){
-    toggleBtn.innerHTML = 'On';
-  } else {
-    toggleBtn.innerHTML = 'Off';
-  }
-}
-
-
-
 //CLASSES 
 
 class Coffee {
@@ -58,30 +46,65 @@ class CoffeMachine {
   }
   powerUp(){
     if (toggleBtn.innerHTML === 'Off'){
-      alert('Switch on the Coffee Machine, please!')
-      return;
+      alert('Switch on the Coffee Machine, please!');
+      throw new Error(); 
     }
     this.power = true;
-    alert('The coffee is being made, please wait...');
-  }
-  renderMug(){
-    if (input.value === 'single espresso') {
-      const outputText = document.getElementById('output-text');
-      outputText.innerHTML = 'Single Espresso';
-      const table = document.getElementById('output');
-      table.classList.add('sml-mug');
+    alert('Your coffee is being made, please wait...');
     }
-  }
-  renderCoffee(){
-    // lines of code
+  createCoffee(){
+    let newInput = input.value;
+    let obj = JSON.parse(newInput);
+    newInput = new Coffee(`${obj.name}`, `${obj.volume}`);
+    console.log(newInput);
+    return newInput;
   }
 }
 
-function makeCoffee() {
-  let coffeMachine = new CoffeMachine(200, false);
-  coffeMachine.powerUp();
-  coffeMachine.renderMug();
+// SWITCH
+
+function toggle() {
+  if (toggleBtn.innerHTML === 'Off'){
+    toggleBtn.innerHTML = 'On';
+  } else {
+    toggleBtn.innerHTML = 'Off';
+  }
+}
+
+let coffeeMachine = new CoffeMachine(200, false);
+console.log(coffeeMachine);
+
+function makeCoffee(event) {
+  event.preventDefault();
+  coffeeMachine.powerUp();
+  let newCoffee = coffeeMachine.createCoffee();
+  let newMug = newCoffee.volume < 80 ? new SmallMug('smallMug', '90') : new LargeMug('largeMug', '120');
+  console.log(newCoffee, newMug);
+  renderCoffee(newMug, newCoffee);
+  coffeeMachine.water = coffeeMachine.water - newCoffee.volume;
+  console.log(coffeeMachine.water);
+  if (coffeeMachine.water <= 0) {
+    let input2 = prompt('please add more water (amount should be between 100 and 200):');
+    if (input2 == 0 || input2 == '' || input2 > 200 || input2 < 100) {
+      error();
+    }
+    coffeeMachine.water = coffeeMachine.water + parseInt(input);
+    console.log(coffeeMachine.water);
+  }
 };
+
+function renderCoffee(mug, coffee) {
+ let output = document.getElementById('output');
+  output = mug.size < 100 ? output.innerHTML = `<div class="sml-mug"><div class="${coffee.name}">${coffee.volume} ml</div></div>` :
+   output.innerHTML = `<div class="lrg-mug"><div class="${coffee.name}">${coffee.volume} ml</div></div>`;
+  return output;
+}
+
+function error() {
+  let output2 = document.getElementById('output');
+  output2.innerHTML = `<div class="sml-mug">no water, no coffee</div>`
+}
+
 
 // EVENTLISTENERS
 
